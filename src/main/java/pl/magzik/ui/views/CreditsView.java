a1427acee69729afe9052dd10e2f8142cc3a7bdf
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-08-27 15:05:32 +0200

    Credits View Rebuild
    -- Rebuilded CreditsView.

--- src/main/java/pl/magzik/ui/views/CreditsView.java
+++ src/main/java/pl/magzik/ui/views/CreditsView.java
@@ -3,6 +3,7 @@ package pl.magzik.ui.views;
 import pl.magzik.ui.components.Utility;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
@@ -13,12 +14,24 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+/**
+ * The {@code CreditsView} class represents a UI component that displays credits information within the application.
+ * This view includes descriptions, author information, and a hyperlink to the author's GitHub profile.
+ * The layout is managed using a {@link GridBagLayout} to ensure flexible placement of labels and other components.
+ */
 public class CreditsView extends AbstractView {
 
+    /**
+     * Constructs a new {@code CreditsView} and initializes the UI components.
+     */
     public CreditsView() {
         initialize();
     }
 
+    /**
+     * Initializes the components and layout for the credits view.
+     * This method sets up the main panel with various labels and a hyperlink, using a {@link GridBagLayout} for positioning.
+     */
     private void initialize() {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
@@ -29,96 +42,133 @@ public class CreditsView extends AbstractView {
         c.weighty = 0;
         c.gridy = 0;
 
-        JLabel desc = new JLabel("view.credits.desc.1");
-        desc.setFont(Utility.fontHelveticaBold);
-        desc.setHorizontalAlignment(SwingConstants.CENTER);
-        desc.setBorder(new EmptyBorder(10, 0, 0, 0));
-
-        JLabel desc2 = new JLabel("view.credits.desc.2");
-        desc2.setFont(Utility.fontHelveticaPlain);
-        desc2.setHorizontalAlignment(SwingConstants.CENTER);
-        desc2.setBorder(new CompoundBorder(
-                new MatteBorder(0,0,1,0, Color.GRAY),
-                new EmptyBorder(0, 0, 10, 0)
-        ));
-
-        JLabel authorLabel = new JLabel("view.credits.label.author");
-        authorLabel.setBorder(new CompoundBorder(
-                new MatteBorder(1, 0, 0, 0, Color.gray),
+        addLabels(
+            mainPanel,
+            c,
+            createLabel(
+                "view.credits.desc.1",
+                Utility.fontHelveticaBold,
                 new EmptyBorder(10, 0, 0, 0)
-        ));
-        authorLabel.setFont(Utility.fontHelveticaBold);
-        authorLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel authorText = new JLabel("view.credits.author");
-        authorText.setBorder(new CompoundBorder(
-                new MatteBorder(0, 0, 1, 0, Color.gray),
-                new EmptyBorder(0, 0, 10, 0)
-        ));
-        authorText.setFont(Utility.fontHelveticaPlain);
-        authorText.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel githubLabel = new JLabel("view.credits.label.github");
-        githubLabel.setBorder(new CompoundBorder(
-                new MatteBorder(1, 0, 0, 0, Color.gray),
-                new EmptyBorder(10, 0, 0, 0)
-        ));
-        githubLabel.setFont(Utility.fontHelveticaBold);
-        githubLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel githubHyperlink = new JLabel("view.credits.github.link");
-        githubHyperlink.setFont(Utility.fontHelveticaPlain);
-        githubHyperlink.setBorder(new CompoundBorder(
-                new MatteBorder(0, 0, 1, 0, Color.gray),
-                new EmptyBorder(0, 0, 10, 0)
-        ));
-        githubHyperlink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        githubHyperlink.setHorizontalAlignment(SwingConstants.CENTER);
-        githubHyperlink.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                try {
-                    Desktop.getDesktop().browse(new URI("https://github.com/maksik997"));
-                } catch (IOException | URISyntaxException ex) {
-                    throw new RuntimeException(ex); // todo for now
-                }
-            }
-        });
-
-        JLabel versionLabel = new JLabel("general.version");
-        versionLabel.setBorder(new CompoundBorder(
-                new MatteBorder(1, 0, 0, 0, Color.GRAY),
-                new EmptyBorder(5, 0, 5, 0)
-        ));
-        versionLabel.setFont(Utility.fontSmallHelveticaBold);
-        versionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        mainPanel.add(desc, c);
-        c.gridy++;
-        mainPanel.add(desc2, c);
-        c.gridy++;
-        c.weighty = 1;
-        mainPanel.add(Box.createVerticalBox(), c);
-        c.gridy++;
-        c.weighty = 0;
-        mainPanel.add(authorLabel, c);
-        c.gridy++;
-        mainPanel.add(authorText, c);
-        c.gridy++;
-        mainPanel.add(githubLabel, c);
-        c.gridy++;
-        mainPanel.add(githubHyperlink, c);
-        c.gridy++;
-        c.weighty = 1;
-        mainPanel.add(Box.createVerticalBox(), c);
-        c.gridy++;
-        c.weighty = 0;
-        mainPanel.add(versionLabel, c);
-        c.gridy++;
-        mainPanel.add(Box.createVerticalGlue(), c);
+            ),
+            createLabel(
+                "view.credits.desc.2",
+                Utility.fontHelveticaPlain,
+                new CompoundBorder(
+                        new MatteBorder(0,0,1,0, Color.GRAY),
+                        new EmptyBorder(0, 0, 10, 0)
+                )
+            ),
+            null,
+            createLabel(
+                "view.credits.label.author",
+                Utility.fontHelveticaBold,
+                new CompoundBorder(
+                        new MatteBorder(1, 0, 0, 0, Color.gray),
+                        new EmptyBorder(10, 0, 0, 0)
+                )
+            ),
+            createLabel(
+                "view.credits.author",
+                Utility.fontHelveticaPlain,
+                new CompoundBorder(
+                        new MatteBorder(0, 0, 1, 0, Color.gray),
+                        new EmptyBorder(0, 0, 10, 0)
+                )
+            ),
+            createLabel(
+                "view.credits.label.github",
+                Utility.fontHelveticaBold,
+                new CompoundBorder(
+                        new MatteBorder(1, 0, 0, 0, Color.gray),
+                        new EmptyBorder(10, 0, 0, 0)
+                )
+            ),
+            createHyperlink(
+                "view.credits.github.link",
+                Utility.fontHelveticaPlain,
+                new CompoundBorder(
+                        new MatteBorder(0, 0, 1, 0, Color.gray),
+                        new EmptyBorder(0, 0, 10, 0)
+                ),
+                "https://github.com/maksik997"
+            ),
+            null,
+            createLabel(
+                "general.version",
+                Utility.fontSmallHelveticaBold,
+                new CompoundBorder(
+                        new MatteBorder(1, 0, 0, 0, Color.GRAY),
+                        new EmptyBorder(5, 0, 5, 0)
+                )
+            )
+        );
 
         add(mainPanel, BorderLayout.CENTER);
     }
 
-    
+    /**
+     * Adds a series of labels to the specified panel using the provided {@link GridBagConstraints}.
+     * Labels can be {@code null} to create vertical spacing between sections.
+     *
+     * @param mainPanel The panel to which the labels will be added.
+     * @param c         The {@link GridBagConstraints} used to layout the labels.
+     * @param labels    The labels to be added to the panel.
+     */
+    private void addLabels(JPanel mainPanel, GridBagConstraints c, JLabel... labels) {
+        for (JLabel label : labels) {
+            if (label == null) {
+                c.gridy++;
+                c.weighty = 1;
+                mainPanel.add(Box.createVerticalBox(), c);
+                c.weighty = 0;
+                continue;
+            }
+            c.gridy++;
+            mainPanel.add(label, c);
+        }
+
+        c.gridy++;
+        mainPanel.add(Box.createVerticalGlue(), c);
+    }
+
+    /**
+     * Creates a {@link JLabel} with the specified text, font, and border.
+     *
+     * @param text   The text to display in the label.
+     * @param font   The font to use for the label's text.
+     * @param border The border to apply to the label.
+     * @return A configured {@link JLabel}.
+     */
+    private JLabel createLabel(String text, Font font, Border border) {
+        JLabel label = new JLabel(text);
+        label.setFont(font);
+        label.setBorder(border);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        return label;
+    }
+
+    /**
+     * Creates a {@link JLabel} that acts as a hyperlink. When clicked, it opens the specified URL in the default web browser.
+     *
+     * @param text   The text to display in the label.
+     * @param font   The font to use for the label's text.
+     * @param border The border to apply to the label.
+     * @param link   The URL to open when the label is clicked.
+     * @return A configured {@link JLabel} that acts as a hyperlink.
+     */
+    private JLabel createHyperlink(String text, Font font, Border border, String link) {
+        JLabel label = createLabel(text, font, border);
+        label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI(link));
+                } catch (IOException | URISyntaxException ignored) { }
+            }
+        });
+
+        return label;
+    }
 }
